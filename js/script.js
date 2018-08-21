0b6d7cacff2991d3ea205310c66ab3c26862f90c
Jason Sikes <31295952+TechRancher@users.noreply.github.com>
2018-08-21 11:08:06 -0500

    Fixed Bugs
    I fixed the bugs and got the program working.

--- js/script.js
+++ js/script.js
@@ -2,79 +2,171 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-
-// Add variables that store DOM elements you will need to reference and/or manipulate
-const students = document.getElementsByClassName(".student-item");
+// Delaring global variables
+const students = document.querySelectorAll(".student-item");
+const pageElement = document.querySelector(".page");
+const pageHeader = document.querySelector(".page-header");
+const divElement = document.createElement("div");
 const perPage = 10;
-const totalPages = Math.ceil(students.length / perPage);
-const div = document.createElement("div");
+let searchResult = students;
 
-
-
-
-
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-
-const hideStudents = (studentList) => {
-  studentList = students;
-  for (let i=0; i<studentList.length; i++){
-    studentList[i].style.display = "none";
+// Create a function to hide all the students.
+const hideStudents = () => {
+  for (let i = 0; i < students.length; i++) {
+    students[i].style.display = "none";
   }
-}
+};
 
-const showPage = (pageNum, studentList) => {
+/* This function calls on the hideStudents() and will display the studentList 
+onto a page broke down to 10 perPage */
+const showPage = pageNum => {
   hideStudents();
-  let endCount = pageNum * perPage;
-  let startCount = endCount - perPage - 1;
-  for(let i=0; i<studentList.length; i++){
-    if(i <= endCount  && i >= startCount) {
-      studentList[i].style.display = "block";
+  noSearchResultsHide();
+
+  if (searchResult.length > 0) {
+    const startCount = (pageNum - 1) * perPage;
+    const endCount =
+      startCount + perPage > searchResult.length
+        ? searchResult.length
+        : startCount + perPage;
+
+    for (let i = startCount; i < endCount; i++) {
+      searchResult[i].style.display = "list-item";
     }
+  } else {
+    noSearchResultsShow();
   }
-}
+};
 
+/* This function is for the search. It takes in the value of the search form and looks for a match
+in the student's name which is an h3 element. If there is a match it will then push it into an array then 
+the array will be displayed. */
+const search = (searchValue = "") => {
+  if (searchValue !== "") {
+    searchResult = [];
+    for (let i = 0; i < students.length; i++) {
+      const studentfilterName =
+        students[i]
+          .querySelector("h3")
+          .innerText.toLowerCase()
+          .indexOf(searchValue.toLowerCase()) > -1;
+      if (studentfilterName) {
+        searchResult.push(students[i]);
+      }
+      // this will hide the page btn at the bottom if searchResult is less than 10
+      if (searchResult.length <= perPage) {
+        divElement.style.display = "none";
+      } else {
+        divElement.style.display = "block";
+      }
+    }
+  } else {
+    searchResult = students;
+  }
+  showPage(1);
+  appendPageLinks();
+};
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
+// This function resets the search form and page to page 1.
+const searchReset = () => {
+  onload
+  search();
+  document.querySelector(".student-search input").value = "";
+};
 
-const appendPageLinks = (studentList) => {
-  const pageClass = document.getElementsByClassName("page")[0];
-  const div = document.createElement("div");
-  div.className = "pagination";
-  pageClass.appendChild(div);
+// This function creates the pagination links
+const appendPageLinks = () => {
+  const totalPages = Math.ceil(searchResult.length / perPage);
   const ulCreate = document.createElement("ul");
-  div.appendChild(ulCreate);
-  for(let i=0; i<totalPages; i++){
+  
+  // this removes the additional page btns durning a search
+  while (divElement.firstChild) {
+    divElement.removeChild(divElement.firstChild);
+  }
+  // loop through the pagebtns
+  for (let i = 1; i <= totalPages; i++) {
     const liCreate = document.createElement("li");
     ulCreate.appendChild(liCreate);
     const anchorCreate = document.createElement("a");
     anchorCreate.href = "#";
-    anchorCreate.textContent = i + 1;
+    anchorCreate.textContent = i;
     liCreate.appendChild(anchorCreate);
-    const activeAnchor = document.querySelector("a");
-    activeAnchor.className = "active";
-    const activePage = document.querySelector("a");
-    // Add functionality to the pagination buttons so that they show and hide the correct items
-    // Tip: If you created a function above to show/hide list items, it could be helpful here
-    div.addEventListener("click", (event) => {
-      event.preventDefault();
+    
+    
+
+    // Add an eventListener to handle the pageNum Button
+    liCreate.addEventListener("click", event => {
       const pageButton = event.target.textContent;
+      const activeAnchor = document.querySelectorAll("a");
       showPage(pageButton);
-      for(let i=0; i<activePage.length; i++) {
-        activePage[i].classList.remove("active");
+      for (let i = 0; i < activeAnchor.length; i++) {
+        activeAnchor[i].classList.remove("active");
         event.target.classList.add("active");
       }
     });
   }
+  // finish appending the rest of the pagination elements
+  divElement.classList.add("pagination");
+  divElement.appendChild(ulCreate);
+  pageElement.appendChild(divElement);
+  // This function makes the page btn become active on page load
+  const activeA = document.querySelector(".pagination li a");
+  activeA.classList.add("active");
 };
 
-showPage(1, students);
+// This function creates the search form elements.
+const searchOption = () => {
+  const searchDiv = document.createElement("div");
+  searchDiv.classList.add("student-search");
+  searchDiv.innerHTML = `<input placeholder="Search for Student"><button>Search</button>`;
+  pageHeader.appendChild(searchDiv);
+};
+
+// This function create the no search results when no students are found.
+const noSearchResults = () => {
+  const noResultsDiv = document.createElement("div");
+  noResultsDiv.classList.add("no-search-results");
+  noResultsDiv.style.display = "none";
+  noResultsDiv.innerHTML = `Sorry. No student found with that name. <a href="#">Reset Search</a>`;
+  pageElement.appendChild(noResultsDiv);
+};
+
+// This function showes the text for when no search results were found.
+const noSearchResultsShow = () => {
+  document.querySelector(".no-search-results").style.display = "block";
+};
+
+// This function hides the text for when no search results were found.
+const noSearchResultsHide = () => {
+  document.querySelector(".no-search-results").style.display = "none";
+};
+
+// Calles the functions.
+searchOption();
+noSearchResults();
+showPage(1);
 appendPageLinks();
 
 
+// Key up event listener to handle the key up event on the search form.
+pageHeader.addEventListener("keyup", event => {
+  if (event.target && event.target.nodeName == "INPUT") {
+    search(event.target.value);
+  }
+});
 
+// Button event listener to handle the click event on the search button.
+pageHeader.addEventListener("click", event => {
+  if (event.target && event.target.nodeName == "BUTTON") {
+    search(event.target.previousElementSibling.value);
+  }
+});
 
-
-
-
-
+// Reset search event listener to handle the click event in the no search results.
+pageElement.addEventListener("click", event => {
+  if (event.target && event.target.nodeName == "A" && event.target.parentElement.className === "no-search-results") {
+    // this reloads the page to start the search over again.
+    document.location.reload(true);
+    searchReset();
+  }
+});
